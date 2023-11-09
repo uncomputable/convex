a574ec836a05c241d2a8d55435dda03e731301f2
Christian Lewe <ch_lewe@agdsn.me>
2023-11-09 15:43:34 +0100

    Nix: Fix nixpkgs to 23.05

--- default.nix
+++ default.nix
@@ -1,4 +1,6 @@
-let
-  pkgs = import <nixpkgs> {};
-in
-  with pkgs; callPackage ./convex.nix {}
+{ pkgs ? import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/4ecab3273592f27479a583fb6d975d4aba3486fe.tar.gz";
+    sha256 = "10wn0l08j9lgqcw8177nh2ljrnxdrpri7bp0g7nvrsn9rkawvlbf";
+  }) {}
+}:
+with pkgs; callPackage ./convex.nix {}
