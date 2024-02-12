16abc3c128e6c31460b4f8f42aab7a7369d365ea
Christian Lewe <ch_lewe@agdsn.me>
2024-02-12 21:40:20 +0900

    Update input parsing
    Handle newlines and whitespaces. Update and code formatting.

--- convex.py
+++ convex.py
@@ -6,38 +6,57 @@ def parse_string(string: str) -> List[int]:
     """
     209577bda6bf4b5804bd46f8621580dd6d4e8bfa2d190e1c50e932492baca07d
     """
-    return [int(string[i:i+2], 16) for i in range(0, len(string), 2)]
+    hex_string = string \
+        .replace("\n", "") \
+        .replace(" ", "")
+    hex_strings = [hex_string[i:i+2] for i in range(0, len(hex_string), 2)]
+    return [int(s, 16) for s in hex_strings if s]
 
 
 def parse_c(c: str) -> List[int]:
     """
     0x209577bdu, 0xa6bf4b58u, 0x04bd46f8u, 0x621580ddu, 0x6d4e8bfau, 0x2d190e1cu, 0x50e93249u, 0x2baca07du
     """
-    array = c.replace(" ", "").split(",")
-    return [int(x, 16) for sublist in [[j[2:4], j[4:6], j[6:8], j[8:10]] for j in array] for x in sublist]
+    four_hex_strings = c \
+        .replace("\n", "") \
+        .replace(" ", "") \
+        .split(",")
+    hex_strings = [s[2:-1][i:i+2] for s in four_hex_strings for i in range(0, 8, 2)]
+    return [int(s, 16) for s in hex_strings if s]
 
 
 def parse_coq(coq: str) -> List[int]:
     """
     32%Z; 149%Z; 119%Z; 189%Z; 166%Z; 191%Z; 75%Z; 88%Z; 4%Z; 189%Z; 70%Z; 248%Z; 98%Z; 21%Z; 128%Z; 221%Z; 109%Z; 78%Z; 139%Z; 250%Z; 45%Z; 25%Z; 14%Z; 28%Z; 80%Z; 233%Z; 50%Z; 73%Z; 43%Z; 172%Z; 160%Z; 125%Z
     """
-    return [int(x) for x in coq[1:-1].replace("%Z", "").replace(" ", "").split(";")]
+    hex_strings = coq \
+        .replace("\n", "") \
+        .replace(" ", "") \
+        .replace("%Z", "") \
+        .split(";")
+    return [int(s) for s in hex_strings if s]
 
 
 def parse_rust(rust: str) -> List[int]:
     """
     0x20, 0x95, 0x77, 0xbd, 0xa6, 0xbf, 0x4b, 0x58, 0x04, 0xbd, 0x46, 0xf8, 0x62, 0x15, 0x80, 0xdd, 0x6d, 0x4e, 0x8b, 0xfa, 0x2d, 0x19, 0x0e, 0x1c, 0x50, 0xe9, 0x32, 0x49, 0x2b, 0xac, 0xa0, 0x7d
     """
-    array_string = rust[1:-1].replace(" ", "")
-    array = array_string.split(",")
-    return [int(x[2:4], 16) for x in array]
+    hex_strings = rust \
+        .replace("\n", "") \
+        .replace(" ", "") \
+        .split(",")
+    return [int(s[2:], 16) for s in hex_strings if s]
 
 
 def parse_json(json: str) -> List[int]:
     """
     32, 149, 119, 189, 166, 191, 75, 88, 4, 189, 70, 248, 98, 21, 128, 221, 109, 78, 139, 250, 45, 25, 14, 28, 80, 233, 50, 73, 43, 172, 160, 125
     """
-    return [int(x) for x in json.split(",")]
+    hex_strings = json \
+        .replace("\n", "") \
+        .replace(" ", "") \
+        .split(",")
+    return [int(s) for s in hex_strings if s]
 
 
 def format_string(lst: List[int]) -> str:
