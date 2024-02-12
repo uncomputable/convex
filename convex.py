b43f8f6b12fe52e5604bf92922067c2dcbe6e44e
Christian Lewe <ch_lewe@agdsn.me>
2024-02-13 00:05:38 +0900

    Fix lints

--- convex.py
+++ convex.py
@@ -1,3 +1,7 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
 import argparse
 import re
 import sys
@@ -35,10 +39,10 @@ def parse_string(string: str) -> Optional[List[int]]:
     return [int(s, 16) for s in hex_strings if s]
 
 
-def parse_c(c: str) -> Optional[List[int]]:
-    if not is_c(c):
+def parse_c(string: str) -> Optional[List[int]]:
+    if not is_c(string):
         return None
-    four_hex_strings = c \
+    four_hex_strings = string \
         .replace("\n", "") \
         .replace(" ", "") \
         .split(",")
@@ -46,10 +50,10 @@ def parse_c(c: str) -> Optional[List[int]]:
     return [int(s, 16) for s in hex_strings if s]
 
 
-def parse_coq(coq: str) -> Optional[List[int]]:
-    if not is_coq(coq):
+def parse_coq(string: str) -> Optional[List[int]]:
+    if not is_coq(string):
         return None
-    hex_strings = coq \
+    hex_strings = string \
         .replace("\n", "") \
         .replace(" ", "") \
         .replace("%Z", "") \
@@ -57,20 +61,20 @@ def parse_coq(coq: str) -> Optional[List[int]]:
     return [int(s) for s in hex_strings if s]
 
 
-def parse_rust(rust: str) -> Optional[List[int]]:
-    if not is_rust(rust):
+def parse_rust(string: str) -> Optional[List[int]]:
+    if not is_rust(string):
         return None
-    hex_strings = rust \
+    hex_strings = string \
         .replace("\n", "") \
         .replace(" ", "") \
         .split(",")
     return [int(s[2:], 16) for s in hex_strings if s]
 
 
-def parse_json(json: str) -> Optional[List[int]]:
-    if not is_json(json):
+def parse_json(string: str) -> Optional[List[int]]:
+    if not is_json(string):
         return None
-    hex_strings = json \
+    hex_strings = string \
         .replace("\n", "") \
         .replace(" ", "") \
         .split(",")
@@ -80,14 +84,13 @@ def parse_json(json: str) -> Optional[List[int]]:
 def parse_auto(string: str) -> Optional[List[int]]:
     if is_c(string):
         return parse_c(string)
-    elif is_coq(string):
+    if is_coq(string):
         return parse_coq(string)
-    elif is_rust(string):
+    if is_rust(string):
         return parse_rust(string)
-    elif is_json(string):
+    if is_json(string):
         return parse_json(string)
-    else:
-        return parse_string(string)
+    return parse_string(string)
 
 
 def format_string(lst: List[int]) -> str:
@@ -95,7 +98,8 @@ def format_string(lst: List[int]) -> str:
 
 
 def format_c(lst: List[int]) -> str:
-    return ", ".join([f"0x{lst[i]:02x}{lst[i+1]:02x}{lst[i+2]:02x}{lst[i+3]:02x}u" for i in range(0, len(lst), 4)])
+    four_int_values = [(lst[i], lst[i+1], lst[i+2], lst[i+3]) for i in range(0, len(lst), 4)]
+    return ", ".join([f"0x{a:02x}{b:02x}{c:02x}{d:02x}u" for (a, b, c, d) in four_int_values])
 
 
 def format_coq(lst: List[int]) -> str:
@@ -155,17 +159,17 @@ def main():
 
     # Format output
     if args.to_rust:
-        s = format_rust(lst)
+        output = format_rust(lst)
     elif args.to_coq:
-        s = format_coq(lst)
+        output = format_coq(lst)
     elif args.to_json:
-        s = format_json(lst)
+        output = format_json(lst)
     elif args.to_c:
-        s = format_c(lst)
+        output = format_c(lst)
     else:
-        s = format_string(lst)
+        output = format_string(lst)
 
-    print(s)
+    print(output)
 
 
 if __name__ == "__main__":
@@ -173,12 +177,14 @@ if __name__ == "__main__":
 
 
 class TestConvex(unittest.TestCase):
+    # pylint: disable=line-too-long
     LIST = [32, 149, 119, 189, 166, 191, 75, 88, 4, 189, 70, 248, 98, 21, 128, 221, 109, 78, 139, 250, 45, 25, 14, 28, 80, 233, 50, 73, 43, 172, 160, 125]
     STRING = "209577bda6bf4b5804bd46f8621580dd6d4e8bfa2d190e1c50e932492baca07d"
     C = "0x209577bdu, 0xa6bf4b58u, 0x04bd46f8u, 0x621580ddu, 0x6d4e8bfau, 0x2d190e1cu, 0x50e93249u, 0x2baca07du"
     COQ = "32%Z; 149%Z; 119%Z; 189%Z; 166%Z; 191%Z; 75%Z; 88%Z; 4%Z; 189%Z; 70%Z; 248%Z; 98%Z; 21%Z; 128%Z; 221%Z; 109%Z; 78%Z; 139%Z; 250%Z; 45%Z; 25%Z; 14%Z; 28%Z; 80%Z; 233%Z; 50%Z; 73%Z; 43%Z; 172%Z; 160%Z; 125%Z"
     RUST = "0x20, 0x95, 0x77, 0xbd, 0xa6, 0xbf, 0x4b, 0x58, 0x04, 0xbd, 0x46, 0xf8, 0x62, 0x15, 0x80, 0xdd, 0x6d, 0x4e, 0x8b, 0xfa, 0x2d, 0x19, 0x0e, 0x1c, 0x50, 0xe9, 0x32, 0x49, 0x2b, 0xac, 0xa0, 0x7d"
     JSON = "32, 149, 119, 189, 166, 191, 75, 88, 4, 189, 70, 248, 98, 21, 128, 221, 109, 78, 139, 250, 45, 25, 14, 28, 80, 233, 50, 73, 43, 172, 160, 125"
+    # pylint: enable=line-too-long
 
     def test_is_string(self):
         self.assertTrue(is_string(self.STRING))
